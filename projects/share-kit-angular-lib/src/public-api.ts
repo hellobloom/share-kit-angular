60736b3c9f7871e6f18ad7abe836b00be9ed5865
Bhanu Teja P <pbteja1998@gmail.com>
2019-05-24 23:00:40 +0530

    empty repo

--- projects/share-kit-angular-lib/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of share-kit-angular-lib
- */
-
-export * from './lib/share-kit-angular-lib.service';
-export * from './lib/share-kit-angular-lib.component';
-export * from './lib/share-kit-angular-lib.module';
