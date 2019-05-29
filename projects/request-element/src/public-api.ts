7384bbfc713911d72c5d5c9205041bd0509b95ff
Bhanu Teja P <pbteja1998@gmail.com>
2019-05-29 20:44:21 +0530

    Share Kit for Angular (#2)
    * Initial Commit
    
    * initialize lib
    
    * add RequestElement component
    
    * add storybook
    
    * some tslint fixes
    
    * do not render RequestElement component in app
    
    * add test for the component
    
    * update README file
    
    * update README
    
    * update README
    
    * Update README.md
    
    * Update README.md
    
    * empty repo
    
    * restructure project
    
    * add LICENCE
    
    * Update README.md
    
    * use storybook-addon-knobs
    
    * make package as public
    
    * make @bloomprotocol/share-kit as dependency instead of peerDependency

---
+++ projects/request-element/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of request-element
+ */
+
+export * from '@bloomprotocol/share-kit/dist/src/types';
+export * from '@bloomprotocol/share-kit/dist/src/util';
+
+export * from './lib/request-element.component';
+export * from './lib/request-element.module';
+
